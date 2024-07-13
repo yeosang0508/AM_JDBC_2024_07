6fd350592f316079004e9f82b3ccefc04ca144a2
yeosang0508 <yeosang0508@gmail.com>
2024-07-13 23:46:08 +0900

    write, list에 DBUtil, SecSql 적용

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,8 +1,12 @@
 package org.koreait;
 
+import org.koreait.util.DBUtil;
+import org.koreait.util.SecSql;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -63,79 +67,32 @@ public class App {
             System.out.print("내용 : ");
             String body = sc.nextLine();
 
-            PreparedStatement pstmt = null;
+            SecSql sql = new SecSql();
 
-            try {
-                String sql = "INSERT INTO article ";
-                sql += "SET regDate = NOW(),";
-                sql += "updateDate = NOW(),";
-                sql += "title = '" + title + "',";
-                sql += "`body` = '" + body + "';";
+            sql.append("INSERT INTO article");
+            sql.append("SET regDate = NOW(),");
+            sql.append("title = ?,", title);
+            sql.append("body = ?,", body);
 
-                pstmt = conn.prepareStatement(sql);
+            int id = DBUtil.insert(conn, sql);
 
-                int affectedRow = pstmt.executeUpdate();
-
-                System.out.println(affectedRow + "열에 적용됨");
-
-            } catch (SQLException e) {
-                System.out.println("에러 2 : " + e);
-            } finally {
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+            System.out.println(id + "번 글이 생성되었습니다.");
 
         } else if (cmd.equals("article list")) {
             System.out.println("==목록==");
-
-            PreparedStatement pstmt = null;
-            ResultSet rs = null;
-
             List<Article> articles = new ArrayList<>();
 
-            try {
-                String sql = "SELECT * ";
-                sql += "FROM article ";
-                sql += "ORDER BY id DESC;";
+                SecSql sql = new SecSql();
+                sql.append("SELECT * ");
+                sql.append("FROM article");
+                sql.append("ORDER BY id DESC");
 
+               List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn,sql);
 
-                pstmt = conn.prepareStatement(sql);
-                rs = pstmt.executeQuery(sql);
+               for(Map<String, Object> articleMap : articleListMap) {
+                   articles.add(new Article(articleMap));
+               }
 
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String regDate = rs.getString("regDate");
-                    String updateDate = rs.getString("updateDate");
-                    String title = rs.getString("title");
-                    String body = rs.getString("body");
-
-                    Article article = new Article(id, regDate, updateDate, title, body);
-
-                    articles.add(article);
-                }
-            } catch (SQLException e) {
-                System.out.println("에러 3 : " + e);
-            } finally {
-                try {
-                    if (rs != null && !rs.isClosed()) {
-                        rs.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
 
             if (articles.size() == 0) {
                 System.out.println("게시글 없습니다.");
--- src/main/java/org/koreait/Article.java
+++ src/main/java/org/koreait/Article.java
@@ -1,5 +1,7 @@
 package org.koreait;
 
+import java.util.Map;
+
 public class Article {
     private int id;
     private String RegDate;
@@ -15,6 +17,13 @@ public class Article {
         this.body = body;
     }
 
+    public Article(Map<String, Object> articleMap){
+        this.id = (int) articleMap.get("id");
+        this.RegDate = (String)articleMap.get("RegDate");
+        this.UpdateDate = (String) articleMap.get("UpdateDate");
+        this.title = (String) articleMap.get("title");
+        this.body = (String) articleMap.get("body");
+    }
     @Override
     public String toString() {
         return "Article{" +
